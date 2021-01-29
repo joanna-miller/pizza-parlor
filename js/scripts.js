9c2b72bc523f4f834ce97cfa5414f999f0d71781
Jo <joannadawnmiller@gmail.com>
2021-01-29 18:57:30 -0500

    new functionality will not be ready in time for project submission so I moved additional functions to another branch to work on at another time

--- js/scripts.js
+++ js/scripts.js
@@ -1,34 +1,3 @@
-// Business Logic for Order ---------
-function Order() {
-  this.pizzas = {};
-  this.currentId = 0;
-}
-
-Order.prototype.assignId = function() {
-  this.currentId += 1;
-  return this.currentId;
-}
-
-Order.prototype.addToOrder = function(pizza) {
-  pizza.id = this.assignId();
-  this.pizzas[pizza.id] = pizza;
-} 
-
-Order.prototype.findPizza = function(id) {
-  if (this.pizzas[id] != undefined) {
-    return this.pizzas[id];
-  }
-  return false;
-}
-
-Order.prototype.deletePizza = function(id) {
-  if (this.pizzas[id] === undefined) {
-    return false;
-  }
-  delete this.pizzas[id];
-  return true;
-}
-
 // Business Logic for Pizza ---------
 
 function Pizza(price, size, toppings) {
@@ -36,8 +5,6 @@ function Pizza(price, size, toppings) {
   this.size = size;
   this.toppings = toppings;
 }
-
-let pizza = new Pizza(0, "small", [])
 
 Pizza.prototype.priceCalc = function () {
   if (this.size === "small") {
@@ -58,20 +25,9 @@ Pizza.prototype.addToppings = function () {
 }
 
 //User Interface Logic ---------
-let order = new Order();
-
-function displayPizzaDetails(orderToDisplay) {
-  let pizzaList = $("ul#cart");
-  let htmlForPizzaInfo = "";
-  Object.keys(orderToDisplay).forEach(function(key) {
-    const pizza = orderToDisplay.findPizza(key);
-    htmlForPizzaInfo += "<li> id=" + pizza.id + ">" + pizza.size + "</li>"
-  });
-  pizzaList.html(htmlForPizzaInfo);
-}; 
 
 $(document).ready(function() {
-  let newPizza = new Pizza(0, "",[]);
+  
   $("form#pizza-builder").submit(function(event) {
     event.preventDefault();
     const pizzaToppings = [];
@@ -79,18 +35,11 @@ $(document).ready(function() {
     $("input:checkbox[name=topping]:checked").each(function(){
       pizzaToppings.push(parseInt($(this).val()));
     }); 
-    newPizza.size = pizzaSize
-    newPizza.toppings = pizzaToppings;
-    newPizza.priceCalc();
-    newPizza.addToppings();
-    $("#pizza-price").text(newPizza.price);
+    let pizza = new Pizza(0, pizzaSize, pizzaToppings);
+    pizza.priceCalc();
+    pizza.addToppings();
+    $("#pizza-price").text(pizza.price);
     $("#result").show();
-  })
-  $("#add").click(function() {
-    order.addToOrder(newPizza);
-    console.log(order);
-    displayPizzaDetails(order);
-    $("#result").hide();
   })
 });
  
